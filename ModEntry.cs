f2e032fcdd2cead20d06cd87bbbd80df9cbd28cb
gitvic <grikvn@gmail.com>
2025-09-01 23:20:39 -0700

    09/01/2025
    spouseRoom_ now works as far as I've tested. Regular afternoon does not, function won't run for whatever reason. May be due to how I implemented it but I need to do more debugging. Also intending to patch out setUpForOutdoorPatioActivity, marriageDuties, arriveAtFarmHouse, and checkForMarriageDialogue - that should be it in the game but will plan more if needed.

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Linq;
 using HarmonyLib;
 using Microsoft.Xna.Framework;
+using Netcode;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -12,21 +17,33 @@ using StardewValley.Locations;
 namespace MarriageDialogueUnlocker;
 
 /// <summary>The mod entry point.</summary>
-internal sealed class ModEntry : Mod
+internal class ModEntry : Mod
+
 {
+    private static IMonitor YOOO;
+
+    // call this method from your Entry class
+    internal static void Initialize(IMonitor monitor)
+    {
+        YOOO = monitor;
+    }
     /// <summary>The mod entry point, called after the mod is first loaded.</summary>
     /// <param name="helper">Provides simplified APIs for writing mods.</param>
     public override void Entry(IModHelper helper)
     {
         Harmony harmony = new(ModManifest.UniqueID);
+        Initialize(Monitor);
+        YOOO.Log($"patches started!");
+        // PREFIXES
         harmony.Patch(
-            original: AccessTools.Method(typeof(StardewValley.NPC), nameof(NPC.setRandomAfternoonMarriageDialogue)),
-            transpiler: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.Afternoon_Transpile))
+            original: AccessTools.Method(typeof(NPC), nameof(NPC.setRandomAfternoonMarriageDialogue)),
+            prefix: new HarmonyMethod(typeof(ModEntry), nameof(setRandomAfternoonMarriageDialogue_prefix))
         );
         harmony.Patch(
-            original: AccessTools.Method(typeof(StardewValley.NPC), nameof(NPC.setSpouseRoomMarriageDialogue)),
-            transpiler: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.SpouseRoom_Transpile))
+            original: AccessTools.Method(typeof(NPC), nameof(NPC.setSpouseRoomMarriageDialogue)),
+            prefix: new HarmonyMethod(typeof(ModEntry), nameof(setSpouseRoomMarriageDialogue_prefix))
         );
+        YOOO.Log("patches done! <3");
         /*
          * NEXT ITERATION
          * 
@@ -37,16 +54,77 @@ internal sealed class ModEntry : Mod
         */
     }
 
-    public static IEnumerable<CodeInstruction> Afternoon_Transpile(IEnumerable<CodeInstruction> instructions)
+    public static bool setRandomAfternoonMarriageDialogue_prefix(NPC __instance, ref NetBool ___hasSaidAfternoonDialogue, int time, GameLocation location, bool countAsDailyAfternoon = false)
     {
-        CodeMatcher match = new CodeMatcher(instructions);
-        MethodInfo 
+        YOOO.Log($"setting dialogue!!");
+        try
+        {
+            if (__instance.Name == "Krobus" && Game1.shortDayNameFromDayOfSeason(Game1.dayOfMonth) == "Fri" || ___hasSaidAfternoonDialogue.Value)
+                return true;
+            if (countAsDailyAfternoon)
+                ___hasSaidAfternoonDialogue.Value = true;
+            Random daySaveRandom = Utility.CreateDaySaveRandom((double)time);
+            int heartLevelForNpc = __instance.getSpouse().getFriendshipHeartLevelForNPC(__instance.Name);
+            switch (location)
+            {
+                case FarmHouse _:
+                    if (!daySaveRandom.NextBool())
+                        break;
+                    if (heartLevelForNpc < 9)
+                    {
+                        __instance.currentMarriageDialogue.Clear();
+                        __instance.addMarriageDialogue("MarriageDialogue", randomAfternoonDialogueRewrite(__instance.Name, daySaveRandom, (daySaveRandom.NextDouble() < (double)heartLevelForNpc / 11.0 ? "Neutral_" : "Bad_"), false));
+                        break;
+                    }
+                    if (daySaveRandom.NextDouble() < 0.05)
+                    {
+                        __instance.currentMarriageDialogue.Clear();
+                        __instance.addMarriageDialogue("MarriageDialogue", randomAfternoonDialogueRewrite(__instance.Name, daySaveRandom, $"{Game1.currentSeason}_{__instance.Name}"));
+                        break;
+                    }
+                    if (heartLevelForNpc >= 10 && daySaveRandom.NextBool() || heartLevelForNpc >= 11 && daySaveRandom.NextDouble() < 0.75 || heartLevelForNpc >= 12 && daySaveRandom.NextDouble() < 0.95)
+                    {
+                        __instance.currentMarriageDialogue.Clear();
+                        __instance.addMarriageDialogue("MarriageDialogue", randomAfternoonDialogueRewrite(__instance.Name, daySaveRandom, "Good_", false));
+                    }
+                    __instance.currentMarriageDialogue.Clear();
+                    __instance.addMarriageDialogue("MarriageDialogue", randomAfternoonDialogueRewrite(__instance.Name, daySaveRandom, "Neutral_", false));
+                    break;
+                case Farm _:
+                    __instance.currentMarriageDialogue.Clear();
+                    if (daySaveRandom.NextDouble() < 0.2)
+                    {
+                        __instance.addMarriageDialogue("MarriageDialogue", randomAfternoonDialogueRewrite(__instance.Name, daySaveRandom, $"Outdoor_{__instance.Name}", false));
+                        break;
+                    }
+                    __instance.addMarriageDialogue("MarriageDialogue", randomAfternoonDialogueRewrite(__instance.Name, daySaveRandom, "Outdoor_"));
+                    break;
+            }
+            return false;
+        }
+        catch (Exception e)
+        {
+            YOOO.Log("no dialogue for you :( .. \n{e}", LogLevel.Error);
+            return true;
+        }
+        
 
     }
-
-    public static IEnumerable<CodeInstruction> SpouseRoom_Transpile(IEnumerable<CodeInstruction> instructions)
+    public static bool setSpouseRoomMarriageDialogue_prefix(NPC __instance)
     {
-
+        YOOO.Log("setting spouse room dialogue!!");
+        try
+        {
+            Random random = Utility.CreateDaySaveRandom();
+            __instance.currentMarriageDialogue.Clear();
+            __instance.addMarriageDialogue("MarriageDialogue", randomAfternoonDialogueRewrite(__instance.Name, random, "spouseRoom_"));
+            return false;
+        }
+        catch (Exception e)
+        {
+            YOOO.Log("no spouse room dialogue for you :( .. \n{e}", LogLevel.Error);
+            return true;
+        }
     }
     /*
      * NEXT ITERATION
@@ -56,26 +134,58 @@ internal sealed class ModEntry : Mod
 
     }
     */
+    
     /// <summary> Rewrite of the dialogue thing in 1.6.15 </summary>
     /// <param name="npc"> Current NPC to be able to use their functions. </param>
     /// <param name="daySaveRandom_Game"> The random function already created in the function to use. </param>
     /// <param name="prefixDialogue"> The prefix of dialogue to match for. </param>
     /// <param name="generalNameToggle"> Whether to look for the NPC's name in the general marriage XNB or not. </param>
-    public void randomAfternoonDialogueRewrite(NPC npc, Random daySaveRandom_Game, string prefixDialogue, bool generalNameToggle = true)
+    public static string randomAfternoonDialogueRewrite(string npcName, Random daySaveRandom_Game, string prefixDialogue, bool generalNameToggle = true)
     {
-        Dictionary<string, string> dialogueDictionaryNPCSpecific = Game1.content.Load<Dictionary<string, string>>($"Characters\\NPC\\MarriageDialogue"+ npc.Name);
-        Dictionary<string, string> dialogueDictionaryGeneral = Game1.content.Load<Dictionary<string, string>>($"Characters\\NPC\\MarriageDialogue");
-        List<string> dialogueDictionaryAll = new List<string>(dialogueDictionaryNPCSpecific.Keys);
-        List<string> dDGeneral = new List<string>(dialogueDictionaryGeneral.Keys);
-        if (generalNameToggle)
+        try
         {
-            // Default to looking for name specific ones here. Turns this off if we're looking for more dialogue in the general file (used for date specific dialogue, mostly).
-            dDGeneral.GroupBy(x => x.Contains(npc.Name));
-        }
-        dialogueDictionaryAll.AddRange(dDGeneral);
-        dialogueDictionaryAll.GroupBy(x => x.Contains(prefixDialogue)).Select(g => g.First()).ToList();
+            // Load dictionary and only take its keys
+            string asset1 = "Characters\\Dialogue\\MarriageDialogue";
+            string asset2 = asset1 + npcName;
+            Dictionary<string, string> dialogueDictionaryNPCSpecific = Game1.content.Load<Dictionary<string, string>>(asset2);
+            Dictionary<string, string> dialogueDictionaryGeneral = Game1.content.Load<Dictionary<string, string>>(asset1);
+            List<string> dialogueDictionaryAll = new List<string>(dialogueDictionaryNPCSpecific.Keys);
+            List<string> dDGeneral = new List<string>(dialogueDictionaryGeneral.Keys);
 
-        npc.addMarriageDialogue("MarriageDialogue", prefixDialogue + daySaveRandom_Game.Next(dialogueDictionaryAll.Count).ToString());
+            // Get all unique keys from both files
+            dialogueDictionaryAll.AddRange(dDGeneral);
+            dialogueDictionaryAll.Select(g => g.First()).ToList();
+
+            // Find the keys that match our queries
+            List<string> matchedKeys = new List<string>();
+
+            for (int i = 0; i < dialogueDictionaryAll.Count; i++)
+            {
+                if (dialogueDictionaryAll[i].Contains(prefixDialogue))
+                {
+                    if (!generalNameToggle || dialogueDictionaryAll[i].Contains(npcName))
+                    {
+                        matchedKeys.Add(dialogueDictionaryAll[i]);
+                    }
+                }
+            }
+
+            // For debugging purposes
+            YOOO.Log("looked for keys matching the prefix: " + prefixDialogue);
+            YOOO.Log("found " + matchedKeys.Count + " keys!!!");
+            for (int i = 0; i < matchedKeys.Count; i++)
+            {
+                YOOO.Log(matchedKeys[i].ToString());
+            }
+
+            // Return a key from our matched list
+            return matchedKeys[daySaveRandom_Game.Next(matchedKeys.Count)];
+        }
+        catch (Exception ex)
+        {
+            YOOO.Log($"error in the main func, oopsies!! .. \n{ex}", LogLevel.Error);
+            return "patio_"+npcName;
+        }
     }
     /*
      * NEXT ITERATION
--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # DialogueUnlocker
-An SDV 1.6.15 ONLY mod that unlocks marriage dialogues. At the moment I've only begun impllementing unlocked afternoon marriage dialogues and spouse room dialogues, but I plan on unlocking the stuff in MarriageDuties too.
+An SDV 1.6.15 ONLY mod that unlocks marriage dialogues.
 
-## This mod is for 1.6.15 ONLY!!!!
+## Warnings!!!!!!
 This issue has, if I'm correct, been fixed in 1.6.16. Do NOT use this mod for 1.6.16!!!!
 I am unsure if this mod works properly for anything below 1.6.15, if there are compatibility issues then update your game.
 
+This mod overwrites any mods that alter any of the following functions:
+- setSpouseRoomMarriageDialogue
+- setRandomAfternoonMarriageDialogue
+- setUpForOutdoorPatioActivity
+- marriageDuties
+- arriveAtFarmHouse
+- checkForMarriageDialogue
+
+I am not planning on this to remain this way but as of now it is not complatible with those that do (via prefixes).
+
 ## How to use (in the future)
-With the dll in the mod folder, with GMCM, make sure "Insert SMAPI" is selected.
+Place the mod folder into the "Stardew Valley/Mods" folder.
+
 Any entries added via "EditData" with Content Patcher should now be recognized by the game under specific circumstances:
 - the prefixes must match the ones the game uses (ex: outdoor dialogue should begin with "Outdoor_" and spouse room dialogue should begin with "spouseRoom_"
-- dialogue in MarriageDialogues.xnb should either be NAME or DATE specific. This is to prevent dialogue for different characters from showing up
+- dialogue in MarriageDialogues.xnb should either be NAME or DATE specific. This is to prevent dialogue for different characters from showing up when not needed
 - MarriageDialogue[name].xnb can have all the dialogues you want as long as they are prefixed properly for the game
 
 ## Releases
-Releases will be on nexusmods.com (full link available when I'm done with the first working commit).
+Releases will be on nexusmods.com (full link available when I'm done with the first fully working commit).
